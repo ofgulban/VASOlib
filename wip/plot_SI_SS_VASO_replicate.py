e4897cfd1430de990d7c9d3a76d3e58f0d262caf
ofgulban <farukgulban@gmail.com>
2022-04-20 23:56:47 +0200

    wip... adding SI-SS-VASO Mz plots

---
+++ wip/plot_SI_SS_VASO_replicate.py
@@ -0,0 +1,159 @@
+"""Replicate Renzo's thesis Fig. 3.2 Panel C Page 47 from gnuplot source."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# =============================================================================
+# Gray matter (direct translation of gnuplot conditions)
+# =============================================================================
+def stage1_gm(time, T1):
+    """Compute Mz for condition: time < Ti."""
+    return 1 - 2 * np.exp((-time) / T1)
+
+
+def stage2_gm(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Ti2."""
+    return 1 - np.exp((-time+Ti) / T1)
+
+
+def stage3_gm(time, T1, Ti2):
+    """Compute Mz for condition: time < Tr+Tr."""
+    return 1 - np.exp((-time+Tr+Ti2) / T1)
+
+
+def stage4_gm(time, T1, Ti2):
+    """Compute Mz for condition: time < Tr+Tr+Ti."""
+    return 1 - np.exp((-time+Tr+Tr) / T1) * (1 - np.exp((-Tr+Ti2) / T1)+1)
+
+
+def stage5_gm(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Ti2."""
+    return 1 - np.exp((-time+Ti+Tr+Tr) / T1)
+
+
+def stage6_gm(time, T1, Ti2):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr."""
+    return 1 - np.exp((-time+Ti2+Tr+Tr+Tr) / T1)
+
+
+def stage7_gm(time, T1, Ti2):
+    """Compute Mz for condition: time < Tr+Tr+Ti+Tr+Tr."""
+    return 1 - np.exp((-time+Tr+Tr+Tr+Tr) / T1) * (1 - np.exp((-Tr+Ti2) / T1)+1)
+
+
+def stage8_gm(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr+Tr+Ti."""
+    return 1 - np.exp((-time+Ti+Tr+Tr+Tr+Tr) / T1)
+
+
+def stage9_gm(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr+Tr+Tr."""
+    return 1 - np.exp((-time+Ti+Tr+Tr+Tr+Tr+Tr) / T1)
+
+
+# =============================================================================
+# Blood (direct translation of gnuplot conditions)
+# =============================================================================
+def stage1_blood(time, T1):
+    """Compute Mz for condition: time < Ti2+Tr."""
+    return 1 - 2 * np.exp(-time / T1)
+
+
+def stage2_blood(time, T1, Ti2):
+    """Compute Mz for condition: time < Tr+Tr."""
+    return 1 - 1 * np.exp(-(time-1*Tr-Ti2) / T1)
+
+
+def stage3_blood(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Ti2."""
+    return 1 - 2 * np.exp(-(time-2*Tr) / T1)
+
+
+def stage4_blood(time, T1, Ti2):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr."""
+    return 1 - 1 * np.exp(-(time-3*Tr-Ti2) / T1)
+
+
+def stage5_blood(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr+Tr."""
+    return 1 - 2 * np.exp(-(time-4*Tr) / T1)
+
+
+def stage6_blood(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr+Tr+Ti."""
+    return 1 - 2 * np.exp(-(time-4*Tr) / T1)
+
+
+def stage7_blood(time, T1, Ti):
+    """Compute Mz for condition: time < Tr+Tr+Tr+Tr+Tr+Tr."""
+    return 1 - 2 * np.exp(-(time-5*Tr) / T1)
+
+
+# =============================================================================
+# Initial parameters (direct translation of gnuplot conditions)
+# =============================================================================
+T1csf = 5
+T1gm = 1.9
+T1b = 2.1  # steady state blood
+
+Ti = 1.45561
+Ti2 = 1.7
+Tr = 2.
+
+x = 5 * Tr + 0.06
+time = np.linspace(0, x, 1001)
+
+# -----------------------------------------------------------------------------
+# Compute plot data
+# -----------------------------------------------------------------------------
+signal1 = np.zeros(time.shape)
+idx = time < Tr+Tr+Tr+Tr+Tr+Tr
+signal1[idx] = stage9_gm(time[idx], T1gm, Ti)
+idx = time < Tr+Tr+Tr+Tr+Tr+Ti
+signal1[idx] = stage8_gm(time[idx], T1gm, Ti)
+idx = time < Tr+Tr+Ti+Tr+Tr
+signal1[idx] = stage7_gm(time[idx], T1gm, Ti2)
+idx = time < Tr+Tr+Tr+Tr
+signal1[idx] = stage6_gm(time[idx], T1gm, Ti2)
+idx = time < Tr+Tr+Tr+Ti2
+signal1[idx] = stage5_gm(time[idx], T1gm, Ti)
+idx = time < Tr+Tr+Ti
+signal1[idx] = stage4_gm(time[idx], T1gm, Ti2)
+idx = time < Tr+Tr
+signal1[idx] = stage3_gm(time[idx], T1gm, Ti2)
+idx = time < Tr+Ti2
+signal1[idx] = stage2_gm(time[idx], T1gm, Ti)
+idx = time < Ti
+signal1[idx] = stage1_gm(time[idx], T1gm)
+
+signal2 = np.zeros(time.shape)
+idx = time < Tr+Tr+Tr+Tr+Tr+Tr
+signal2[idx] = stage7_blood(time[idx], T1b, Ti)
+idx = time < Tr+Tr+Tr+Tr+Tr+Ti
+signal2[idx] = stage6_blood(time[idx], T1b, Ti)
+idx = time < Tr+Tr+Tr+Tr+Tr
+signal2[idx] = stage5_blood(time[idx], T1b, Ti)
+idx = time < Tr+Tr+Tr+Tr
+signal2[idx] = stage4_blood(time[idx], T1b, Ti2)
+idx = time < Tr+Tr+Tr+Ti2
+signal2[idx] = stage3_blood(time[idx], T1b, Ti)
+idx = time < Tr+Tr
+signal2[idx] = stage2_blood(time[idx], T1b, Ti2)
+idx = time < Ti2+Tr
+signal2[idx] = stage1_blood(time[idx], T1b)
+
+# =============================================================================
+# Plotting
+# =============================================================================
+plt.plot(time, signal1, linewidth=2, color="blue")
+plt.plot(time, signal2, linewidth=2, color="red")
+
+plt.title("SI-SS-VASO")
+plt.xlabel("Time [s]")
+plt.ylabel(r"$M_z$")
+plt.ylim([-1, 1])
+plt.grid(True)
+plt.legend(["Gray Matter", "Once Inverted Blood"])
+
+plt.show()
