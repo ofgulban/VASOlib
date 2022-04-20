9ddda574deff731e523f7470d6b3049fcbb5fd03
ofgulban <farukgulban@gmail.com>
2022-04-20 13:05:38 +0200

    wip...

--- wip/plot_VASO.py
+++ wip/plot_VASO.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
 
 
 # =============================================================================
@@ -18,7 +17,7 @@ def stage2(time, T1, Ti):
 
 def stage3(time, T1, Ti):
     """Stage 3: time < Tr+Tr+Ti."""
-    return 1 - np.exp((-time+Tr+Tr) / T1) * (1 + (1 - np.exp((-(Tr+Tr) + Ti) / T1)))
+    return 1 - np.exp((-time+Tr+Tr)/T1) * (1 + (1 - np.exp((-(Tr+Tr)+Ti) / T1)))
 
 
 def stage4(time, T1, Ti):
@@ -34,6 +33,23 @@ def stage5(time, T1, Ti):
 def stage6(time, T1, Ti):
     """Stage 6: time < Tr+Tr+Tr+Tr+Tr+Tr."""
     return 1 - np.exp((-x+Ti+Tr+Tr+Tr+Tr) / T1)
+
+
+# =============================================================================
+# def Mz_after_90deg(time, M0, T1):
+#     """Longitudinal relaxation."""
+#     return M0 - M0 * np.exp(-time/T1)
+#
+#
+# def Mz_after_180deg(time, T1, M0):
+#     """Whatever."""
+#     return M0 - (2*M0) * np.exp(-time/T1)
+
+
+def Mz(time, M0_equi, M0_init, FA_rad, T1):
+    """Longitudinal magnetization."""
+    # return M0 - M0 * (1 - np.cos(FA_rad)) * np.exp(-time/T1)
+    return M0_equi - (M0_equi - M0_equi * np.cos(FA_rad)) * np.exp(-time/T1)
 
 
 # =============================================================================
@@ -58,13 +74,30 @@ signal1[time < Tr+Tr] = stage2(time[time < Tr+Tr], T1b, Ti)
 signal1[time < Ti] = stage1(time[time < Ti], T1b)
 
 # -----------------------------------------------------------------------------
+# Prepare condition array
+cond = np.full(time.shape, 2)
+idx1 = time % (Tr*2) < Ti  # Stages after 180 deg pulse
+cond[idx1] = 1
+
+# Prepare signal array
 signal2 = np.zeros(time.shape)
-signal2[time < Tr+Tr+Tr+Tr+Tr+Tr] = stage6(time[time < Tr+Tr+Tr+Tr+Tr+Tr], T1gm, Ti)
-signal2[time < Tr+Tr+Ti+Tr+Tr] = stage5(time[time < Tr+Tr+Ti+Tr+Tr], T1gm, Ti)
-signal2[time < Tr+Tr+Tr+Tr] = stage4(time[time < Tr+Tr+Tr+Tr], T1gm, Ti)
-signal2[time < Tr+Tr+Ti] = stage3(time[time < Tr+Tr+Ti], T1gm, Ti)
-signal2[time < Tr+Tr] = stage2(time[time < Tr+Tr], T1gm, Ti)
-signal2[time < Ti] = stage1(time[time < Ti], T1gm)
+
+# Prepare initial parameters
+M0 = 1.
+
+for i, t in enumerate(time):
+    t %= 2*Tr
+    if i == 0:  # Handle first signal separately
+        signal2[i] = Mz(time=t, M0_equi=M0, M0_init=1., FA_rad=np.deg2rad(180), T1=T1b)
+    elif t < Ti:
+        if cond[i] != cond[i-1]:  # Update M0 upon condition switch
+            M0 = Mz(time=2*Tr+Ti, M0_equi=M0, M0_init=1.,
+                    FA_rad=np.deg2rad(90), T1=T1b)
+        signal2[i] = Mz(time=t, M0_equi=M0, M0_init=1.,
+                        FA_rad=np.deg2rad(180), T1=T1b)
+    else:
+        signal2[i] = Mz(time=t-Ti, M0_equi=M0, M0_init=1.,
+                        FA_rad=np.deg2rad(90), T1=T1b)
 
 plt.plot(time, signal1)
 plt.plot(time, signal2)
